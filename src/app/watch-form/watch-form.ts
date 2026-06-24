7728fb03e061a9d43710721e064a0dab8fa0df3b
parista_k <parista_k@f112.ub.lan>
2026-06-24 10:55:21 +0800

    Feat: Implement watch-form component and watch service

--- src/app/watch-form/watch-form.ts
+++ src/app/watch-form/watch-form.ts
@@ -1,9 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Watch } from '../watch';
 
 @Component({
   selector: 'app-watch-form',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './watch-form.html',
   styleUrl: './watch-form.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WatchForm {}
+export class WatchForm {
+  private formBuilder = inject(FormBuilder);
+  watchService = inject(Watch);
+
+  editingId = signal<string | null>(null);
+
+  watchForm = this.formBuilder.nonNullable.group({
+    title: ['', Validators.required],
+    episode: ['', Validators.required, Validators.min(1)],
+    season: ['', Validators.required, Validators.min(1)],
+    description: ['', Validators],
+    status: ['', Validators]
+  });
+
+  onSubmit() {
+    if (this.watchForm.valid){
+      const formData = this.watchForm.getRawValue();
+
+      if(this.editingId()) {
+        this.watchService.updateWatch(this.editingId()!, formData).subscribe(() => {
+          this.watchService.fetchWatch();
+          this.cancelEdit();
+        });
+      } else {
+        this.watchService.saveWatch(formData).subscribe(() => {
+          this.watchService.fetchWatch();
+          this.watchForm.reset();
+          console.log('Movie saved successfully!');
+        });
+      }
+    }
+  }
+
+  startEdit(watch: any) {
+    this.editingId.set(watch._id);
+    this.watchForm.patchValue({
+      title: watch.title,
+      episode: watch.episode,
+      season: watch.season,
+      description: watch.description,
+      status: watch.status 
+    });
+  }
+
+  cancelEdit() {
+    this.editingId.set(null);
+    this.watchForm.reset();
+  }
+
+  deleteWatch(id: string) {
+    if (confirm("Are you sure you want to release this Movie")) {
+      this.watchService.deleteWatch(id).subscribe(() => {
+        this.watchService.fetchWatch();
+      })
+    }
+  }
+}
--- src/app/watch.ts
+++ src/app/watch.ts
@@ -1,6 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
 
 @Injectable({
   providedIn: 'root',
 })
-export class Watch {}
+export class Watch {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:3000/api/watch';
+
+  watchList = signal<any[]>([]);
+
+  fetchWatch() {
+    this.http.get<any[]>(this.apiUrl).subscribe((data) => this.watchList.set(data));
+  }
+
+  saveWatch(data: any) {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  deleteWatch(id: string){
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  updateWatch(id: string, data: any) {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
+  }
+}
